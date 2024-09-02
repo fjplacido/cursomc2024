417642f2e2844e32f7cbab3f2367e6eccbd0f606
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2024-09-02 16:18:27 -0300

    Primeiro commit

---
+++ src/main/java/br/com/jsolutions/cursomc2024/Cursomc2024Application.java
@@ -0,0 +1,13 @@
+package br.com.jsolutions.cursomc2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jsolutions/cursomc2024/Cursomc2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jsolutions.cursomc2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursomc2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

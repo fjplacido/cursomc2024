ec19e5f0c6e08ed11f985e16a2e2af82e975d7f8
Francisco José Plácido da Cunha <fj.placido@gmail.com>
2024-10-20 21:47:49 -0300

    Criando operação de instanciação

--- src/main/java/br/com/jsolutions/cursomc2024/Cursomc2024Application.java
+++ src/main/java/br/com/jsolutions/cursomc2024/Cursomc2024Application.java
@@ -1,13 +1,30 @@
 package br.com.jsolutions.cursomc2024;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.jsolutions.cursomc2024.domain.Categoria;
+import br.com.jsolutions.cursomc2024.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Cursomc2024Application {
+public class Cursomc2024Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2024Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
